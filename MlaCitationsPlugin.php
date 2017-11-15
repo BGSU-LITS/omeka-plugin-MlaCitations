f5573d831f73e4796686f372fa7c07e34622f680
John Kloor <john@kloor.org>
2017-11-15 10:52:59 -0500

    Add URL to citation and refactor complexity.

--- MlaCitationsPlugin.php
+++ MlaCitationsPlugin.php
@@ -34,47 +34,12 @@ class MlaCitationsPlugin extends Omeka_Plugin_AbstractPlugin
             );
         }
 
-
-        // Strip formatting and remove empty creator elements.
         $creators = array_filter(
             array_map('trim', array_map('strip_formatting', $creators))
         );
 
         if ($creators) {
-            switch (count($creators)) {
-                case 1:
-                    $creator = $this->mlaName($creators[0]);
-                    break;
-
-                case 2:
-                    $creator = __(
-                        '%1$s and %2$s',
-                        $this->mlaName($creators[0]),
-                        $this->mlaName($creators[1], false)
-                    );
-
-                    break;
-
-                case 3:
-                    $creator = __(
-                        '%1$s, %2$s, and %3$s',
-                        $this->mlaName($creators[0]),
-                        $this->mlaName($creators[1], false),
-                        $this->mlaName($creators[2], false)
-                    );
-
-                    break;
-
-                default:
-                    $creator = __(
-                        '%s et al',
-                        $this->mlaName($creators[0])
-                    );
-
-                    break;
-            }
-
-            $citation .= $this->mlaPeriod($creator). ' ';
+            $citation .= $this->mlaPeriod($this->mlaCreators($creators)) . ' ';
         }
 
         $title = trim(strip_formatting(
@@ -82,13 +47,13 @@ class MlaCitationsPlugin extends Omeka_Plugin_AbstractPlugin
         ));
 
         if ($title) {
-            $citation .= '&#8220;'. $this->mlaPeriod($title). '&#8221; ';
+            $citation .= '&#8220;' . $this->mlaPeriod($title) . '&#8221; ';
         }
 
         $siteTitle = trim(strip_formatting(option('site_title')));
 
         if ($siteTitle) {
-            $citation .= '<i>'. $this->mlaPeriod($siteTitle). '</i> ';
+            $citation .= '<i>' . $this->mlaPeriod($siteTitle) . '</i> ';
         }
 
         $author = trim(strip_formatting(option('author')));
@@ -97,14 +62,50 @@ class MlaCitationsPlugin extends Omeka_Plugin_AbstractPlugin
             $published = strtotime($args['item']->modified);
 
             if ($published > 0) {
-                $author .= ', '. $this->mlaDate($published);
+                $author .= ', ' . $this->mlaDate($published);
             }
 
-            $citation .= $this->mlaPeriod($author). ' ';
+            $url = record_url($args['item'], null, true);
+
+            if ($url) {
+                $url = preg_replace('{^https?://}', '', $url);
+                $author .= ', ' . $this->mlaPeriod($url);
+            }
+
+            $citation .= $this->mlaPeriod($author) . ' ';
         }
 
-        $citation .= 'Web. '. $this->mlaPeriod($this->mlaDate(time()));
+
+        $citation .= 'Accessed ' . $this->mlaPeriod($this->mlaDate(time()));
         return $citation;
+    }
+
+    private function mlaCreators($creators)
+    {
+        switch (count($creators)) {
+            case 1:
+                return $this->mlaNamePrimary($creators[0]);
+
+            case 2:
+                return __(
+                    '%1$s and %2$s',
+                    $this->mlaNamePrimary($creators[0]),
+                    $this->mlaNameSecondary($creators[1])
+                );
+
+            case 3:
+                return __(
+                    '%1$s, %2$s, and %3$s',
+                    $this->mlaNamePrimary($creators[0]),
+                    $this->mlaNameSecondary($creators[1]),
+                    $this->mlaNameSecondary($creators[2])
+                );
+        }
+
+        return __(
+            '%s et al',
+            $this->mlaNamePrimary($creators[0])
+        );
     }
 
     private function mlaDate($time)
@@ -112,25 +113,30 @@ class MlaCitationsPlugin extends Omeka_Plugin_AbstractPlugin
         $month = date('F', $time);
 
         if (strlen($month) > 4) {
-            $month = substr($month, 0, 3). '.';
+            $month = substr($month, 0, 3) . '.';
         }
 
-        return date('j ', $time). $month. date(' Y', $time);
+        return date('j ', $time) . $month . date(' Y', $time);
     }
 
-    private function mlaName($name, $comma = true)
+    private function mlaNamePrimary($name)
     {
         if (preg_match('/[^\d\s\w()\[\];:,.\/-]/', $name)) {
             return $name;
         }
 
-        if ($comma) {
-            if (strpos($name, ',') !== false) {
-                return $name;
-            }
+        if (strpos($name, ',') !== false) {
+            return $name;
+        }
 
-            $parts = preg_split('/\s+/', $name);
-            return array_pop($parts). ', '. implode(' ', $parts);
+        $parts = preg_split('/\s+/', $name);
+        return array_pop($parts) . ', ' . implode(' ', $parts);
+    }
+
+    private function mlaNameSecondary($name)
+    {
+        if (preg_match('/[^\d\s\w()\[\];:,.\/-]/', $name)) {
+            return $name;
         }
 
         if (strpos($name, ',') === false) {
@@ -138,12 +144,12 @@ class MlaCitationsPlugin extends Omeka_Plugin_AbstractPlugin
         }
 
         $parts = preg_split('/,\s*/', $name, 2);
-        return $parts[1]. ' '. $parts[0];
+        return $parts[1] . ' ' . $parts[0];
     }
 
     private function mlaPeriod($text)
     {
-        if (substr($text, -1, 1) != '.') {
+        if (substr($text, -1, 1) !== '.') {
             $text .= '.';
         }
 
